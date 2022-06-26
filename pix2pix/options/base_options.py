fe9dc4ae3ec78311da6009b71f826e3c6f12df43
StevenLi <75846119+StevenLi-phoenix@users.noreply.github.com>
2022-06-26 15:08:45 +0800

    Update base_options.py
    add opt cpu to override GPU:0 on CPU device(no cuda)

--- pix2pix/options/base_options.py
+++ pix2pix/options/base_options.py
@@ -72,6 +72,7 @@ class BaseOptions():
         parser.add_argument('--Final', action='store_true')
         parser.add_argument('--colorize_results', action='store_true')
         parser.add_argument('--max_res', type=float, default=np.inf)
+        parser.add_argument('--cpu', action='store_true')
 
         self.initialized = True
         return parser
@@ -146,11 +147,13 @@ class BaseOptions():
         # since cuda is currently using cpu this is ineffective
         str_ids = opt.gpu_ids.split(',')
         opt.gpu_ids = []
-        for str_id in str_ids:
-            id = int(str_id)
-            if id >= 0:
-                opt.gpu_ids.append(id)
-        print(opt.gpu_ids)
+        if not opt.cpu:
+            for str_id in str_ids:
+                id = int(str_id)
+                if id >= 0:
+                    opt.gpu_ids.append(id)
+        else:
+            pass # leave gpu_ids blank
         if len(opt.gpu_ids) > 0:
             torch.cuda.set_device(opt.gpu_ids[0])
         else:
