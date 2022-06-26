fb0780956a6a1f54b2324199359690b69d82110d
StevenLi <75846119+StevenLi-phoenix@users.noreply.github.com>
2022-06-26 14:31:58 +0800

    Update base_options.py

--- pix2pix/options/base_options.py
+++ pix2pix/options/base_options.py
@@ -142,14 +142,16 @@ class BaseOptions():
         self.print_options(opt)
 
         # set gpu ids
-        str_ids = opt.gpu_ids.split(',')
-        opt.gpu_ids = []
-        for str_id in str_ids:
-            id = int(str_id)
-            if id >= 0:
-                opt.gpu_ids.append(id)
-        if len(opt.gpu_ids) > 0:
-            torch.cuda.set_device(opt.gpu_ids[0])
+        # it doesn't work on the CPU version
+        # since cuda is currently using cpu this is ineffective
+        # str_ids = opt.gpu_ids.split(',')
+        # opt.gpu_ids = []
+        # for str_id in str_ids:
+        #     id = int(str_id)
+        #     if id >= 0:
+        #         opt.gpu_ids.append(id)
+        # if len(opt.gpu_ids) > 0:
+        #     torch.cuda.set_device(opt.gpu_ids[0])
 
         self.opt = opt
         return self.opt
